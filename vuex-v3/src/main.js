f3f8a320760b43114221b3c81dbfea13fa43fe11
Matteo Sausto <matteo.sausto@codeland.it>
2022-02-07 17:35:22 +0100

    Start migrate from vue-2 to vue-3

--- vuex-v3/src/main.js
+++ vuex-v3/src/main.js
@@ -1,7 +1,38 @@
 import { createApp } from 'vue';
+import { createStore } from 'vuex';
 
 import App from './App.vue';
 
-// Add Vuex!
+// As the same as the router, we use the store with the createStore part of Vuex
 
-createApp(App).mount('#app');
+const store = createStore({ 
+    state() {
+    return {
+      users: [],
+    };
+  },
+  mutations: {
+    addUser(state, payload) {
+      const newUser = {
+        id: new Date().toISOString(),
+        name: payload.name,
+      };
+      state.users.push(newUser);
+    },
+  },
+  actions: {
+    addUser(context, payload) {
+      context.commit('addUser', payload);
+    },
+  },
+  getters: {
+    users(state) {
+      return state.users;
+    },
+  }
+});
+
+const app = createApp(App)
+app.use(store);
+
+app.mount('#app');
