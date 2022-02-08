6c2168652a5e6c021753cfab6bcb4a003e7d2f98
Matteo Sausto <matteo.sausto@codeland.it>
2022-02-08 11:07:06 +0100

    Composition Api migration

--- composition-api/src/components/AddGoal.vue
+++ composition-api/src/components/AddGoal.vue
@@ -3,6 +3,7 @@
     <form @submit.prevent="addGoal">
       <div>
         <label for="goaltext">Goal</label>
+        <!-- here we don't use the .value to access to the value -->
         <input type="text" id="goaltext" v-model="enteredText" />
       </div>
       <p v-if="invalidInput">Please enter a valid goal text (non-empty).</p>
@@ -12,32 +13,65 @@
 </template>
 
 <script>
+import { ref, watch } from 'vue';
+
 export default {
   emits: ["add-goal"],
-  data() {
-    return {
-      enteredText: "",
-      invalidInput: false,
-    };
-  },
-  methods: {
-    addGoal() {
-      this.invalidInput = false;
-      if (this.enteredText === "") {
-        this.invalidInput = true;
-        return;
-      }
-      this.$emit("add-goal", this.enteredText);
-      this.enteredText = '';
-    },
-  },
-  watch: {
-    invalidInput(val) {
+  setup(_, context){
+    const enteredText = ref('');
+    const invalidInput = ref(false);
+
+    // 2 argument:
+    // 1. the dependency of the watcher
+    // 2. the function to execute when the dependency changes
+    // We can access to the changed value and on the oldValue as second argumnet on the watch callback
+    watch(invalidInput, function(val){
       if (val) {
         console.log("Analytics: Invalid Input");
       }
-    },
-  },
+    });
+
+    function addGoal() {
+      invalidInput.value = false;
+      if (enteredText.value === "") {
+        invalidInput.value = true;
+        return;
+      }
+      // We can emit an event with the context and not the &
+      context.emit("add-goal", enteredText.value);
+      enteredText.value = '';
+    }
+
+    return {
+      enteredText,
+      invalidInput,
+      addGoal
+    }
+  }
+  // data() {
+  //   return {
+  //     enteredText: "",
+  //     invalidInput: false,
+  //   };
+  // },
+  // methods: {
+  //   addGoal() {
+  //     this.invalidInput = false;
+  //     if (this.enteredText === "") {
+  //       this.invalidInput = true;
+  //       return;
+  //     }
+  //     this.$emit("add-goal", this.enteredText);
+  //     this.enteredText = '';
+  //   },
+  // },
+  // watch: {
+  //   invalidInput(val) {
+  //     if (val) {
+  //       console.log("Analytics: Invalid Input");
+  //     }
+  //   },
+  // },
 };
 </script>
 
--- composition-api/src/components/CourseGoals.vue
+++ composition-api/src/components/CourseGoals.vue
@@ -6,6 +6,8 @@
 </template>
 
 <script>
+import { ref, computed } from 'vue';
+
 import GoalsList from "./GoalsList.vue";
 import AddGoal from "./AddGoal.vue";
 
@@ -14,26 +16,58 @@ export default {
     GoalsList,
     AddGoal,
   },
-  data() {
-    return {
-      goals: [],
-    };
-  },
-  computed: {
-    filteredGoals() {
-      return this.goals.filter(
+  // We can pass a props to the setup
+  // setup(props) {
+  setup() {
+    // into setup() we not can access to data with this.
+    // create a variable, but with refs only when vue is loaded and can update the template.
+    const goals = ref([]);
+
+    const filteredGoals = computed(function() {
+      // We need to use .value to access to the value of the refs
+      return goals.value.filter(
         (goal) => !goal.text.includes("Angular") && !goal.text.includes("React")
       );
-    },
-  },
-  methods: {
-    addGoal(text) {
+    });
+
+    // We can add a function inside a function
+    function addGoal(text){
       const newGoal = {
         id: new Date().toISOString(),
         text: text,
       };
-      this.goals.push(newGoal);
-    },
-  },
+      goals.value.push(newGoal);
+    }
+
+    return {
+      // We can use this userGolas as a data source for the GoalsList component
+      // userGoals: goals
+
+      filteredGoals: filteredGoals,
+      addGoal: addGoal
+    };
+
+  }
+  // data() {
+  //   return {
+  //     goals: [],
+  //   };
+  // },
+  // computed: {
+  //   filteredGoals() {
+  //     return this.goals.filter(
+  //       (goal) => !goal.text.includes("Angular") && !goal.text.includes("React")
+  //     );
+  //   },
+  // },
+  // methods: {
+  //   addGoal(text) {
+  //     const newGoal = {
+  //       id: new Date().toISOString(),
+  //       text: text,
+  //     };
+  //     this.goals.push(newGoal);
+  //   },
+  // },
 };
 </script>
